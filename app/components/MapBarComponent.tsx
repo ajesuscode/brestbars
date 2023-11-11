68906ddf8a46859f581f6b2e46daadf813493484
andrew.mixkiv <andrewmixkiv@gmail.com>
2023-11-11 09:41:45 +0100

    feat: added leaflet

--- app/components/MapBarComponent.tsx
+++ app/components/MapBarComponent.tsx
@@ -1,8 +1,5 @@
 "use client";
-import React from "react";
-import Map, { Marker } from "react-map-gl";
-
-import "mapbox-gl/dist/mapbox-gl.css";
+import React, { useEffect } from "react";
 import { BarTypes } from "../types/types";
 
 interface MapBarComponentProps {
@@ -10,32 +7,5 @@ interface MapBarComponentProps {
 }
 
 export const MapBarComponent = ({ bars }: MapBarComponentProps) => {
-    const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-    console.log(mapboxToken);
-    return (
-        <div>
-            <div className="text-zinc-200">MABBOX COMPONENT</div>
-            <Map
-                mapLib={import("mapbox-gl")}
-                mapboxAccessToken={mapboxToken}
-                initialViewState={{
-                    longitude: -20,
-                    latitude: -12,
-                    zoom: 14,
-                }}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-            >
-                {bars.map((bar: BarTypes) => {
-                    <Marker
-                        longitude={bar.location.coordinates[0]}
-                        latitude={bar.location.coordinates[1]}
-                        anchor="bottom"
-                        key={bar.id}
-                    >
-                        <div>X</div>
-                    </Marker>;
-                })}
-            </Map>
-        </div>
-    );
+    return <div></div>;
 };
